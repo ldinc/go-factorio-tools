b85e4565a41ed64d936858ff1ff76c0e235cbc34
ldinc <drogunov.igor@gmail.com>
2025-05-01 18:42:53 +0300

    [0.0.1] pretty output for build mode

--- internal/manager/op.go
+++ internal/manager/op.go
@@ -14,7 +14,7 @@ func (m *Manager) BuildAll() {
 	for _, mod := range m.Mods {
 		zipName := mod.GetZipString()
 
-		fmt.Printf("|> build release [%s]\n\t --- archive: %s\n", mod.Info.Name, zipName)
+		fmt.Printf("|> build release [%s]\n\t --- archive : %s\n", mod.Info.Name, zipName)
 
 		originalByteCount, compressedByteCount, err := mod.Info.ToZip(mod.Dir, m.TargetDir)
 
@@ -22,7 +22,8 @@ func (m *Manager) BuildAll() {
 			panic(err)
 		}
 
-		fmt.Printf("\t --- size: %s -> %s\n", humanize.Bytes(originalByteCount), humanize.Bytes(compressedByteCount))
+		fmt.Printf("\t --- size    : %s -> %s\n", humanize.Bytes(originalByteCount), humanize.Bytes(compressedByteCount))
 		fmt.Printf("\t --- location: [%s]\n", m.TargetDir)
+		fmt.Println()
 	}
 }
